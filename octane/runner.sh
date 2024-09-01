35507632399bbddf5f7ecd1ff9f93586cfb95b07
Muhajirin Ilyas <muhajirinlpu@pm.me>
2024-09-01 09:27:55 +0000

    add option to set workers

--- octane/runner.sh
+++ octane/runner.sh
@@ -13,9 +13,10 @@ else
 fi
 
 echo "run on '$DOCKER_ENV' environment!"
+echo "run with '$DOCKER_WORKERS' workers!"
 
 if [ $DOCKER_ENV == "production" ]; then
-    php artisan octane:start --host=0.0.0.0 --port=80
+    php artisan octane:start --host=0.0.0.0 --port=80 --workers=$DOCKER_WORKERS --task-workers=$DOCKER_WORKERS
 else
     if [ -d node_modules ]; then
         echo "node_modules ok!"
@@ -23,5 +24,5 @@ else
         npm install
     fi
 
-    php artisan octane:start --host=0.0.0.0 --watch --port=80
+    php artisan octane:start --host=0.0.0.0 --watch --port=80 --workers=$DOCKER_WORKERS --task-workers=$DOCKER_WORKERS
 fi
